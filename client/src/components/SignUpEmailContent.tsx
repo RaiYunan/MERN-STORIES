bc4b3996eb9b4a0460b51536b7ea10c7b5eb58a4
RaiYunan <yunanrai433@gmail.com>
2025-12-01 13:14:37 +0545

    feat(auth): add email signup form with zod validation

--- client/src/components/SignUpEmailContent.tsx
+++ client/src/components/SignUpEmailContent.tsx
@@ -1,7 +1,140 @@
-
+import mailLogo from "@/assets/images/mail-logo.png";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const SignUpEmailContent = () => {
-  return <>SignUp Email</>;
+  const formSchema = z.object({
+    name: z
+      .string()
+      .min(2, "Name must be at least 2 characters long.")
+      .max(100, "Name must be less than 100 characters long."),
+    email: z.string().email("Please enter a valid email address."),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string(),
+  });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <>
+      <div className="max-w-full mx-auto">
+        <img src={mailLogo} alt="mail-logo" className=" w-14 h-14" />
+      </div>
+      <h2 className="font-serif text-center text-2xl font-medium">
+        Sign up with email
+      </h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your name" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="mb-3">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email address" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div>
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password again"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex items-center flex-col gap-4">
+            <Button type="submit" className="text-center max-w-fit">
+              Create Account
+            </Button>
+            <button className="underline text-sm">Back to sign up options</button>
+            <p className="text-sm">Already have an account?{" "}<button className="underline">Sign in</button></p>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default SignUpEmailContent;
