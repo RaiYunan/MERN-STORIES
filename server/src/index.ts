28928e8ceca21e824ca1cf4adadaa161abc409e5
RaiYunan <yunanrai433@gmail.com>
2025-12-04 08:46:40 +0545

    setup: initialize TypeScript backend with Express, tsconfig, and base structure

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+
+const app:Express=express()
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello backend setup!");
+});
+
+app.listen(5000, () => console.log("Server running..."));
