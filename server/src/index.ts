0676ddca16419d326a8b50cb2b9222218a054218
RaiYunan <yunanrai433@gmail.com>
2025-12-13 08:59:21 +0545

    fix: cors error/bug fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,12 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app: Express = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser())
 app.use(express.json({ limit: "50mb" }));
 app.use(
